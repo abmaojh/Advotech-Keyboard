12c8b6b476c20f5d231e07a9a0f231b2ba768d8f
Abdulrahman Alhammadi <60799560+abmaojh@users.noreply.github.com>
2024-02-19 17:00:42 -0600

    Add files via upload

--- Latest/AccountView.swift
+++ Latest/AccountView.swift
@@ -1,1 +1,26 @@
+//
+//  AccountView.swift
+//  Keyboard Advotech
+//
+//  Created by Alhammadi, Abdulrahman (UMKC-Student) on 2/19/24.
+//
 
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
